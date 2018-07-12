6530d850e483172e4341ca016672b8103daec622
songweishuai <songweishuai@thunder.com.cn>
2018-07-12 23:10:25 +0800

    初始代码上传

---
+++ app/src/main/java/com/sws/criminalintent/CrimeFragment.java
@@ -0,0 +1,27 @@
+package com.sws.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime=new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+        //View v=inflater.inflate(R.layout.fragment_crime,container,false);
+        //return v;
+    }
+}
